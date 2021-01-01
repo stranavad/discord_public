d85da80f0ab86ad7104ed16e5ffb96ffeee80087
stranavadavid <stranavadavid@stranavadavid-kali.stranavadavid.net>
2021-01-01 16:06:51 +0100

    Discord bot is ready to be deployed

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import discord
 import mysql.connector
 
+mydb = mysql.connector.connect(
+  host="192.46.233.86",
+  user="root",
+  password="<Code><Tech> 127521",
+  database="discord_bot"
+)
+
+mycursor = mydb.cursor()
+
 token_first = "NzkyMTQ0NDUwNjQxMzMwMjU2.X-ZcAg."
 token_second = "FbS-SGkycAxTXCxBap2_gn55Gp0"
 token = token_first + token_second
@@ -10,10 +19,33 @@ commands = dict()
 disChannels = dict()
 proGuilds = dict()
 
+sql_commands = {
+    "add_commands": "INSERT INTO %s (name, value) VALUES (%s, %s)",
+    "delete_command": "DELETE FROM %s WHERE name = %s",
+    "disable_channel": "INSERT INTO %s (id,) VALUES (%s)",
+    "enable_channel": "DELETE FROM %s WHERE id = %s",
+    "add_proguild": "INSERT INTO guilds (id, commands) VALUES (%s, %s)",
+    "delete_proguild": "DELETE FROM guilds WHERE id = %s",
+    "show_tables": "SHOW TABLES",
+    "create_commands_table": "CREATE TABLE %s (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), value VARCHAR(255))",
+    "create_disabled_channels_table": "CREATE TABLE %s (id BIGINT PRIMARY KEY)",
+}
 
-@client.event
-async def on_ready():
-    print("Discord bot is ready")
+
+# MySQL functions
+def add_guild(guild_id):
+    commands_table = "commandsx" + str(guild_id)
+    disabled_channels_table = "disabled_channelsx" + str(guild_id)
+    create_commands_table = f"CREATE TABLE {commands_table} (id BIGINT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), value VARCHAR(255))"
+    create_disabled_channels_table = f"CREATE TABLE {disabled_channels_table} (id BIGINT PRIMARY KEY)"
+
+    mycursor.execute(create_commands_table)
+    mydb.commit()
+    mycursor.execute(create_disabled_channels_table)
+    mydb.commit()
+    mycursor.execute(sql_commands["add_proguild"], (guild_id, 10))
+    mydb.commit()
+    return
 
 
 async def add_command(message, guild_id):
@@ -26,9 +58,13 @@ async def add_command(message, guild_id):
         second = second.strip()
         comms = commands[guild_id]
         comms[first] = second
+        db_guild_id = "commandsx" + str(guild_id)
+        mycursor.execute(f"INSERT INTO {db_guild_id} (name, value) VALUES (%s, %s)", (first, second))
+        mydb.commit()
         await message.channel.send(f"Commands <{first}> with value of <{second}> was added")
     except:
         await message.channel.send("There was an error, when adding the command.")
+
 
 
 async def max_commands(message):
@@ -42,16 +78,97 @@ async def delete_command(message, guild_id):
         comms = commands[guild_id]
         if first in comms:
             comms.pop(first)
+            db_guild_id = "commandsx" + str(guild_id)
+            mycursor.execute(f"DELETE FROM {db_guild_id} WHERE name = '{first}'")
+            mydb.commit()
             # TODO create function for deleting items to speed up the actual code flow, without await
             await message.channel.send(f"Command with value <{first}> vas successfully deleted")
     except:
         await message.channel.send("There was an error, when deleting the command")
 
 
+def update_db():
+    mycursor.execute(sql_commands["show_tables"])
+    result = mycursor.fetchall()
+    for res in result:
+        table_name = res[0]
+        if "commands" in table_name:
+            _, guild_id = table_name.split("x")
+            guild_id = int(guild_id)
+            mycursor.execute(f"SELECT * FROM {table_name}")
+            commands_query = mycursor.fetchall()
+            if commands_query:
+                commands_place = dict()
+                for command in commands_query:
+                    commands_place[command[1]] = command[2]
+                commands[guild_id] = commands_place
+        elif "disabled_channels" in table_name:
+            _, guild_id = table_name.split("x")
+            guild_id = int(guild_id)
+            mycursor.execute(f"SELECT * FROM {table_name}")
+            channels_query = mycursor.fetchall()
+            if channels_query:
+                disabled_channels = list()
+                for channel in channels_query:
+                    disabled_channels.append(channel[0])
+                disChannels[guild_id] = disabled_channels
+        elif table_name == "guilds":
+            mycursor.execute("SELECT * FROM guilds")
+            guilds_results = mycursor.fetchall()
+            if guilds_results:
+                for guilds_result in guilds_results:
+                    guilds.append(guilds_result[0])
+        else:
+            print("Table name " + table_name + " is not recognized")
+
+    print("DB was updated")
+
+
+# TODO adding commands, not working. move creating disChannels and commands to add guild function
+@client.event
+async def on_ready():
+    mycursor.execute(sql_commands["show_tables"])
+    result = mycursor.fetchall()
+    for res in result:
+        table_name = res[0]
+        if "commands" in table_name:
+            _, guild_id = table_name.split("x")
+            guild_id = int(guild_id)
+            mycursor.execute(f"SELECT * FROM {table_name}")
+            commands_query = mycursor.fetchall()
+            if commands_query:
+                commands_place = dict()
+                for command in commands_query:
+                    commands_place[command[1]] = command[2]
+                commands[guild_id] = commands_place
+        elif "disabled_channels" in table_name:
+            _, guild_id = table_name.split("x")
+            guild_id = int(guild_id)
+            mycursor.execute(f"SELECT * FROM {table_name}")
+            channels_query = mycursor.fetchall()
+            if channels_query:
+                disabled_channels = list()
+                for channel in channels_query:
+                    disabled_channels.append(channel[0])
+                disChannels[guild_id] = disabled_channels
+        elif table_name == "guilds":
+            mycursor.execute("SELECT * FROM guilds")
+            guilds_results = mycursor.fetchall()
+            if guilds_results:
+                for guilds_result in guilds_results:
+                    guilds.append(guilds_result[0])
+        else:
+            print("Table name " + table_name + " is not recognized")
+
+    print("Discord bot is ready")
+
+
 @client.event
 async def on_message(message):
     guild_id = message.guild.id
+    # TODO double check statement below, get things to dicts and lists when app is ready
     if guild_id not in guilds:
+        add_guild(guild_id)
         guilds.append(guild_id)
     if guild_id not in commands:
         commands[guild_id] = dict()
@@ -67,16 +184,13 @@ async def on_message(message):
                 # Check the guild's number of commands and maximum commands allowed
                 if guild_id in proGuilds:
                     if len(commands[guild_id]) < proGuilds[guild_id]:
-                        #print("This is a pro guild available commands")
                         await add_command(message, guild_id)
                     elif len(commands[guild_id]) >= proGuilds[guild_id]:
                         await max_commands(message)
                     else:
-                        print("There was some problem with commands number")
+                        print("There is a problem with number of commands")
                 else:
-                    #print(commands[guild_id])
                     if len(commands[guild_id]) < 3:
-                        #print("This is a normal guild with available commands")
                         await add_command(message, guild_id)
                     elif len(commands[guild_id]) >= 3:
                         await max_commands(message)
@@ -101,11 +215,17 @@ async def on_message(message):
                                 await message.channel.send(comms[command])
     elif message.content.lower().strip() == ".disable":
         dch = disChannels[guild_id]
+        db_guild_id = "disabled_channelsx" + str(guild_id)
         if message.channel.id in dch:
+            db_guild_id = "disabled_channelsx" + str(guild_id)
+            mycursor.execute(f"DELETE FROM {db_guild_id} WHERE id = {message.channel.id}")
+            mydb.commit()
             dch.remove(int(message.channel.id))
             await message.channel.send("I have been enabled in this channel")
         else:
             dch.append(int(message.channel.id))
+            mycursor.execute(f"INSERT INTO {db_guild_id} (id) VALUES ({message.channel.id})")
+            mydb.commit()
             await message.channel.send("I have been disabled in this channel")
 
     if guild_id == 792144909125025822:
@@ -115,27 +235,32 @@ async def on_message(message):
             first = first.strip()
             second = second.strip()
             try:
-                print(first)
-                print(second)
                 id = int(first)
                 num = int(second)
                 if id in proGuilds:
                     proGuilds.pop(id)
                 proGuilds[id] = num
+                mycursor.execute("UPDATE guilds SET commands = %s WHERE id = %s", (num, id))
+                mydb.commit()
                 await message.channel.send(f"Guild id {id} is now a pro guild with {num} total commands")
             except:
                 await message.channel.send("Problem with converting id to integer")
         elif ".deleteproguild" in message.content.lower().strip():
             _, first = message.content.lower().split("-")
             try:
-                # TODO check if guild exists first
-                # There is an error, somewhere here, or actually there isn't any, but double check
                 id = int(first.strip())
                 if id in proGuilds:
                     proGuilds.pop(id)
                     commands.pop(id)
+                    # TODO remove all commands from db with that id
+                    # TODO change the guild commands number value back to default -10 commands
+                    db_guild_id = "commandsx" + str(id)
+                    mycursor.execute("UPDATE guilds SET commands = 10 WHERE id = %s", (id,))
+                    mycursor.execute(f"DELETE FROM {db_guild_id}")
+                    mydb.commit()
                     await message.channel.send("Pro guild and commands were deleted")
-                await message.channel.send("There is no pro guild with that id")
+                else:
+                    await message.channel.send("There is no pro guild with that id")
             except:
                 await message.channel.send("There was some problem deleting the pro guild")
 
